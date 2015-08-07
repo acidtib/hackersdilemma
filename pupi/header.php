4f422403c65ef05ac83842274f42d8b4d56cee34
Dan Vera <dan@yovu.co>
2015-08-06 21:06:54 -0400

    icons

--- pupi/header.php
+++ pupi/header.php
@@ -1,14 +1,34 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-  <title>Hackers Dilemma</title>
+<meta charset="utf-8">
+<title>hackers.dilemma</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="http://fonts.googleapis.com/css?family=Kotta+One|Cantarell:400,700" rel="stylesheet" type="text/css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
 
   <meta property="og:title" content="Hackers Dilemma"/>
-  <meta property="og:image" content="http://hackersdilemma.com/pupi/assets/img/facebook.jpg"/>
+  <meta property="og:image" content="http://hackersdilemma.com/pupi/assets/images/facebook.jpg"/>
   <meta property="og:site_name" content="Hackers Dilemma"/>
   <meta property="og:url" content="http://hackersdilemma.com" >
   <meta property="og:description" content=""/>
   <meta property="og:type" content="website" />
+
+  <link rel="apple-touch-icon" sizes="57x57" href="pupi/assets/images/icons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="pupi/assets/images/icons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="pupi/assets/images/icons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="pupi/assets/images/icons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="pupi/assets/images/icons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="pupi/assets/images/icons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="pupi/assets/images/icons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="pupi/assets/images/icons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="pupi/assets/images/icons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="pupi/assets/images/icons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="pupi/assets/images/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="pupi/assets/images/icons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="pupi/assets/images/icons/favicon-16x16.png">
 
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
